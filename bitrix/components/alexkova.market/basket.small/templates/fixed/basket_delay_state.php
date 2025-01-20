2b223151fc19bde0599db9ec82ed3090a181c5b8
Alexey Sklyar <a.sklyar@semicvetic.com>
2025-01-20 13:57:40 +0700

    t29117: Модификация файла стороннего модуля в рамках апдейта версии php.

--- bitrix/components/alexkova.market/basket.small/templates/fixed/basket_delay_state.php
+++ bitrix/components/alexkova.market/basket.small/templates/fixed/basket_delay_state.php
@@ -1,6 +1,18 @@
-<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+<?php /**
+ * @var array $arResult
+ */ ?>
+<i class="fa fa-shopping-cart"></i><br/>
+<?php
+$canBuyItems = isset($arResult["BASKET_ITEMS"]["CAN_BUY"]) && is_array($arResult["BASKET_ITEMS"]["CAN_BUY"])
+    ? count($arResult["BASKET_ITEMS"]["CAN_BUY"])
+    : 0;
 
-	<i class="fa fa-shopping-cart"></i><br />
-        <?$basket_delay_cnt = count($arResult["BASKET_ITEMS"]["CAN_BUY"]) + count($arResult["BASKET_ITEMS"]["DELAY"]);?>
-	<?=$basket_delay_cnt?>
-<!--<br /><span class="bxr-format-price"><?=$arResult["FORMAT_SUMM"]?></span>-->
+$delayItems = isset($arResult["BASKET_ITEMS"]["DELAY"]) && is_array($arResult["BASKET_ITEMS"]["DELAY"])
+    ? count($arResult["BASKET_ITEMS"]["DELAY"])
+    : 0;
+
+$basket_delay_cnt = $canBuyItems + $delayItems;
+?>
+<?= $basket_delay_cnt ?>
+<!--<br /><span class="bxr-format-price"><?= $arResult["FORMAT_SUMM"] ?></span>-->
