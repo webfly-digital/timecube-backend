a17f72cff04634f185ea73704876e816b21ad61b
Alexey Sklyar <a.sklyar@semicvetic.com>
2025-04-02 15:47:59 +0700

    t#30445: Форматирование перед правкой.

--- bitrix/js/ipol.sdek/wndController.js
+++ bitrix/js/ipol.sdek/wndController.js
@@ -20,54 +20,54 @@ function ipol_sdek_wndController(params) {
     init();
 
     // private
-    function init(params){
-        if(typeof(params) === 'undefined')
+    function init(params) {
+        if (typeof (params) === 'undefined')
             params = defaultParams;
 
         var obSetups = {};
-        for(var i in defaultParams)
+        for (var i in defaultParams)
             obSetups[i] = getOpt(i);
 
         wndLink = new BX.CDialog(obSetups);
     }
 
     // service
-    function checkOpt(wat){
-        return (typeof(params[wat]) !== 'undefined');
+    function checkOpt(wat) {
+        return (typeof (params[wat]) !== 'undefined');
     }
 
-    function getOpt(wat){
+    function getOpt(wat) {
         return (checkOpt(wat)) ? params[wat] : defaultParams[wat];
     }
 
     function checkReady(action) {
-        if(self.getWnd())
+        if (self.getWnd())
             return true;
-        else{
-            var title = (getOpt('title')) ? 'of title "'+getOpt('title')+'" ' : '';
-            action    = (typeof(action) === 'undefined') ? '' : ' for '+action;
-            console.warn('Window object '+title+'is not ready'+action);
+        else {
+            var title = (getOpt('title')) ? 'of title "' + getOpt('title') + '" ' : '';
+            action = (typeof (action) === 'undefined') ? '' : ' for ' + action;
+            console.warn('Window object ' + title + 'is not ready' + action);
             return false;
         }
     }
 
     // public
-    this.getWnd = function(){
+    this.getWnd = function () {
         return wndLink;
     };
 
-    this.open   = function(){
-        if(checkReady('open'))
+    this.open = function () {
+        if (checkReady('open'))
             self.getWnd().Show();
     };
 
-    this.setContent = function(content){
-        if(checkReady('adding content'))
+    this.setContent = function (content) {
+        if (checkReady('adding content'))
             self.getWnd().SetContent(content);
     };
 
-    this.close  = function(){
-        if(checkReady('close'))
+    this.close = function () {
+        if (checkReady('close'))
             self.getWnd().Close();
     };
 }
